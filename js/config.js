607c9ec15cb652f714e6e8a5a22e5ad97828213c
Vitaly <altunin.vitaly@gmail.com>
2025-11-28 07:48:55 -0300

    Обнавленная версия

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
     // API настройки
-    API_URL: 'https://d305fd6b1efa.ngrok-free.app',
+    API_URL: 'https://691df5e64861.ngrok-free.app',
     
     // Версия приложения
     VERSION: '4.0.0',
