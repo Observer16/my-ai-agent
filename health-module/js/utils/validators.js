8a3e4c6f9d66a47b60c59e3817718c3a8483fbe0
Vitaly <altunin.vitaly@gmail.com>
2025-12-15 15:15:41 -0300

    Fix v7 симптомы

--- health-module/js/utils/validators.js
+++ health-module/js/utils/validators.js
@@ -25,9 +25,9 @@ const HealthValidators = (function() {
         // Базовые симптомы доступны всем
         const BASE_SYMPTOMS = {
             "общее": ["усталость", "слабость", "температура", "озноб", "потливость", "бессонница", "сонливость"],
-            "голова": ["головная_боль", "головокружение", "мигрень", "давление"],
-            "живот": ["боль_в_животе", "тошнота", "рвота", "диарея", "запор", "вздутие", "изжога"],
-            "прочее": ["боль_в_спине", "боль_в_груди", "кашель", "насморк", "боль_в_горле"]
+            "голова": ["головная боль", "головокружение", "мигрень", "давление", "гул", "потрескивание"],
+            "живот": ["боль в животе", "тошнота", "рвота", "диарея", "запор", "вздутие", "изжога"],
+            "прочее": ["боль в спине", "боль в груди", "кашель", "насморк", "боль в горле"]
         };
 
         if (BASE_SYMPTOMS[category] && BASE_SYMPTOMS[category].includes(name)) {
@@ -38,8 +38,8 @@ const HealthValidators = (function() {
         if (gender === 'female') {
             const FEMALE_SYMPTOMS = {
                 "гинекология": [
-                    "менструальная_боль", "задержка_месячных", "обильные_месячные", "скудные_месячные",
-                    "нерегулярный_цикл", "ПМС", "овуляторная_боль", "выделения", "зуд", "боль_в_груди", "набухание_груди"
+                    "менструальная боль", "задержка месячных", "обильные месячные", "скудные месячные",
+                    "нерегулярный цикл", "ПМС", "овуляторная боль", "выделения", "зуд", "боль в груди", "набухание груди"
                 ]
             };
             if (FEMALE_SYMPTOMS[category] && FEMALE_SYMPTOMS[category].includes(name)) {
@@ -51,7 +51,7 @@ const HealthValidators = (function() {
         if (gender === 'male') {
             const MALE_SYMPTOMS = {
                 "урология": [
-                    "дискомфорт_в_паху", "боль_при_мочеиспускании", "частое_мочеиспускание", "проблемы_с_эрекцией"
+                    "дискомфорт в паху", "боль при мочеиспускании", "частое мочеиспускание", "проблемы с эрекцией"
                 ]
             };
             if (MALE_SYMPTOMS[category] && MALE_SYMPTOMS[category].includes(name)) {
@@ -65,9 +65,9 @@ const HealthValidators = (function() {
     function isSexualActivityValid(activity, gender) {
         if (!activity) return true;
 
-        const SEXUAL_ACTIVITY_OPTIONS_BASE = ["нет", "защищенный_секс", "незащищенный_секс", "самостоятельно"];
-        const SEXUAL_ACTIVITY_OPTIONS_FEMALE = [...SEXUAL_ACTIVITY_OPTIONS_BASE, "активная_роль", "пассивная_роль", "период_овуляции", "во_время_месячных"];
-        const SEXUAL_ACTIVITY_OPTIONS_MALE = [...SEXUAL_ACTIVITY_OPTIONS_BASE, "активная_роль", "пассивная_роль"];
+        const SEXUAL_ACTIVITY_OPTIONS_BASE = ["нет", "защищенный секс", "незащищенный секс", "самостоятельно"];
+        const SEXUAL_ACTIVITY_OPTIONS_FEMALE = [...SEXUAL_ACTIVITY_OPTIONS_BASE, "активная роль", "пассивная роль", "период овуляции", "во время месячных"];
+        const SEXUAL_ACTIVITY_OPTIONS_MALE = [...SEXUAL_ACTIVITY_OPTIONS_BASE, "активная роль", "пассивная роль"];
 
         let validOptions;
         switch(gender) {
