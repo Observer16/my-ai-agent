d0c36943e3ff137fcdd36ddc7541794b5f4e3a32
Vitaly <altunin.vitaly@gmail.com>
2025-12-20 08:27:58 -0300

    Fixing v22 URL

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@
 
 const CONFIG = {
     // API настройки
-    API_URL: 'https://f9e103bcce21.ngrok-free.app',
+    API_URL: 'https://5ade98c43ee3.ngrok-free.app',
     
     // Версия приложения
-    VERSION: '4.1.6',
+    VERSION: '4.1.7',
     
     // Endpoints
     ENDPOINTS: {
