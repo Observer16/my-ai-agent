b01986637d6f8d33a6ecfa153aa3ecdf5728cee9
Vitaly <altunin.vitaly@gmail.com>
2025-12-30 22:41:48 -0300

    Fixing v62

--- js/app-patch.js
+++
@@ -1,9 +0,0 @@
-// app-patch.js - Добавить эту функцию в app.js
-
-/**
- * Открыть настройки
- */
-function openSettings() {
-    tg.HapticFeedback.impactOccurred('light');
-    window.location.href = 'pages/settings.html';
-}
